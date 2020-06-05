972ef66cbad7f8bbc2b72eadf6d4b976c0e21742
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-06-05 17:08:27 +0000

    [Feature] The connection to ssh is created into DBStorage class. modified:   models/__init__.py modified:   models/engine/db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,9 @@
 #all models(class) must be imported to verify if an object exists
 import sqlalchemy
+from sshtunnel import SSHTunnelForwarder
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scopped_session, sessionmaker
-
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models.base_model import Base
 
 class DBStorage:
     """ Administration, manipulation and connection for database """
@@ -14,20 +15,40 @@ class DBStorage:
         """ Create the connection bridge with Database """
         MYSQL_USER = 'chrono_dev_user'
         MYSQL_PWD = 'chrono_dev_pwd'
-        MYSQL_HOST = 'localhost' 
         MYSQL_DB = 'chrono_dev_db'
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(MYSQL_USER, MYSQL_PWD, MYSQL_HOST, MYSQL_DB))
+
+        with SSHTunnelForwarder(
+            ('54.209.158.161', 22),
+            ssh_username='ubuntu',
+            ssh_pkey='~/.ssh/holberton',
+            remote_bind_address=('127.0.0.1', 3306)
+        )  as server:
+            server.start()  # start ssh sever
+            local_port = str(server.local_bind_port)
+            self.__engine= create_engine('mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(MYSQL_USER,
+                                                                                    MYSQL_PWD,
+                                                                                    '127.0.0.1',
+                                                                                    local_port,
+                                                                                    MYSQL_DB),
+                               pool_recycle=1)
 
     def reload(self):
         """ Load all data in objects for sqlAlchemy metadata to be used by the session """
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scopped_session(sess_factory)
+        Session = scoped_session(sess_factory)
         self.__session = Session
 
     def close(self):
         """ remove session of a database """
         self.__session.remove()
+
+    def add(self, obj):
+        """ Add the object to the map of the sqlalchemy orm """
+        self.__session.add(obj)
+
+    def commit(self):
+        self.__session.commit()
 
 
 
