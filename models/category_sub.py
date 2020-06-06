cded377f012800c6747c9b49d9a61f90015b27e1
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-06-06 05:31:33 +0000

    [Feature] Class CategorySub that defines behavior and data for subcategories. new file:   models/category_sub.py

---
+++ models/category_sub.py
@@ -0,0 +1,15 @@
+import models
+from models.base_model import BaseModel, Base
+from models.category_main import CategoryMain
+from models.user import User
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+class CategorySub(BaseModel, Base):
+    """ class tha defines behavior and data for subcategories  """
+    __tablename__ = 'CategorySub'
+    CategoryMain_id = Column(Integer,ForeignKey(CategoryMain.id), nullable=False)
+    User_id = Column(Integer, ForeignKey(User.id), nullable=False)
+
+    def __init__(self, **kwargs):
+        """ constructor sends the values for the object to be created from BaseModel """
+        BaseModel.__init__(self, **kwargs)
--- test_connect.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-import models
-from models.user import User
-
-myuser = User(1, "Camilo", "Mosquera", "2020-08-09", "2020-08-09", "assa@sdcsd", "password", 3000)
-myuser.create()
