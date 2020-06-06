8e05cadfa69dd39845502a3a294ae1209f175964
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-06-06 05:22:55 +0000

    [Feature] Comments for methods. modified:   models/user.py

--- models/user.py
+++ models/user.py
@@ -4,14 +4,18 @@ from sqlalchemy import Column, String, Integer
 #Pending md5 for password
 
 class User(BaseModel, Base):
+    """ Class to define behavior and data for users """
+
     __tablename__ = 'User'
+    last_name = Column(String(45), nullable=False)
     email = Column(String(45), nullable=False)
     password = Column(String(45), nullable=False)
     initial_mileage = Column(Integer, nullable=False)
 
-    def __init__(self, name, last_name, created_at, updated_at, email, password, initial_mileage):
-        BaseModel.__init__(self, name, last_name, created_at, updated_at)
-        self.email = email
-        self.password = password
-        self.initial_mileage = initial_mileage
+    def __init__(self, **kwargs):
+        """ constructor sends the values for the object to be created from BaseModel """
+        BaseModel.__init__(self, **kwargs)
 
+    def login():
+        """ login """
+        pass
