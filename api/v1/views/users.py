c3d266d7a81e97c5a9ebb4a6747f7fa09a00441c
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-06-17 02:40:32 +0000

    [Feature] View users is improved with md5 for password and validation of existence for email. modified:   api/v1/views/users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,6 +3,7 @@ from api.v1.views import app_views
 from flask import jsonify, request, abort
 from models import storage
 from models.user import User
+from models.category_sub import CategorySub
 import hashlib 
 
 # Allowed attributes to modify and create
@@ -23,17 +24,27 @@ def set_obj(obj, **data):
 @app_views.route('/users/', methods=['POST'])
 def create_user():
     """ Creation of a user """
-    #try:
-    data = request.get_json(force=True)
-    for at in attr:
-        if at not in data.keys():
-            return jsonify({"Error": str("Missing " + at)})
-    new_obj = User()
-    set_obj(new_obj, **data)
-    storage.insert(new_obj)
-    return jsonify(new_obj.to_dict())
-    #except Exception:
-     #   abort(404)
+    try:
+        data = request.get_json(force=True)
+        if 'email' in data:
+            exist = validateEmail(data['email'])
+            if exist == 1:
+                return jsonify({"Error": "User email already exists"})
+        for at in attr:
+            if at not in data.keys():
+                return jsonify({"Error": str("Missing " + at)})
+        new_obj = User()
+        set_obj(new_obj, **data)
+        storage.insert(new_obj)
+        #Creation of subcategory Fuel for every new user
+        mycs = CategorySub()
+        mycs.CategoryMain_id = 1
+        mycs.User_id = new_obj.id
+        mycs.name = "Fuel"
+        storage.insert(mycs)
+        return jsonify(new_obj.to_dict())
+    except Exception:
+        abort(404)
 
 @app_views.route('/users/<user_id>/', methods=['PUT'])
 def update_user(user_id):
@@ -70,12 +81,27 @@ def login(user_email):
         data = request.get_json(force=True)
         if 'password' not in data:
             return jsonify({"Error": "Password is required"})
+        #md5 for password
+        password = data['password']
+        m = hashlib.md5()
+        m.update(str.encode(password))
+        data['password'] = m.hexdigest()
         if users[0]['password'] == data['password']:
             if 'password' in users[0]:
                 del users[0]['password']
             return jsonify(users[0])
     except Exception:
         abort(404)
+
+def validateEmail(email):
+    """ Verify if the email exists in database """
+    users = storage.getuser(User, "email", email)
+    if len(users) > 0:
+        return 1
+    else:
+        return 0
+       
+
 
 
 
