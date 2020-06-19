3b7120f29d547f68ad491d70883a4ccc014e1810
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2020-06-19 16:33:39 +0000

    Update views

--- static/scripts/panel.js
+++ static/scripts/panel.js
@@ -1,7 +1,5 @@
 const user_id_panel = sessionStorage.getItem('UserID');
 console.log("UserID: " + user_id_panel);
-
-
 //Alerts panel
 const url_mileage = 'http://34.71.55.165:5000/api/v1/users/' + user_id_panel + '/mileage/';
 $.ajax({
@@ -29,11 +27,9 @@ $.ajax({
             let state = "";
             if (data[i]['color'] == 'green') {
                 state = "<i class='fas fa-circle text-success'></i>";
-            }
-            else if (data[i]['color'] == 'orange') {
+            } else if (data[i]['color'] == 'orange') {
                 state = "<i class='fas fa-circle text-warning'></i>";
-            }
-            else {
+            } else {
                 state = "<i class='fas fa-circle text-danger'></i>";
             }
             $('#alerts_panel').append('<tr> <td>' + data[i]['missing'] + '</td> <td>' + state + '</td> <td>' + data[i]['catsub_name'] + '</td>  </tr>');
