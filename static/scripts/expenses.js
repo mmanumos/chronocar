88ff989ab0b328b84f4295c0206aaede8b108a18
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-06-19 01:21:09 +0000

    [Feature] Styles and test to session. modified:   static/scripts/expenses.js modified:   templates/base.html modified:   templates/panel.html

--- static/scripts/expenses.js
+++ static/scripts/expenses.js
@@ -13,6 +13,7 @@ $.ajax({
 });
 
 const user_id = sessionStorage.getItem('User ID');
+console.log("This is user id: " + user_id);
 const url = 'http://34.71.55.165:5000/api/v1/users/' + user_id + '/expenses/';
 
 $.ajax({
